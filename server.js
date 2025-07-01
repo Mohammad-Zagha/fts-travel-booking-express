f03f5705b55d5fce2a876024afc608755761a66d
Mohammad Zagha <tsu.test11@gmail.com>
2025-07-01 03:04:53 +0300

    cors

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ const writeJsonData = (fileName, newData, uniqueKey = "id") => {
 };
 
 app.use(cors({
-  origin: "http://localhost:5173",
+   origin: "*",
   credentials: true,
 }));
 app.use(morgan("dev"));
